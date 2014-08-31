4e481d3054c0a66471d7d8d962e8d964500d8a9d
dpuleri <daniel.puleri@gmail.com>
2014-08-30 23:45:46 -0400

    Overhauled website to Bootstrap.

---
+++ smoothscroll.js
@@ -0,0 +1,19 @@
+$("a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
